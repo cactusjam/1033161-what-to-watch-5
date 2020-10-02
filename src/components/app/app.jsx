509f2f9eee273e8f0edfc9e3038c1dd5ff50fc4b
Evgenia Alfimova <alfimova.evg@gmail.com>
2020-10-02 14:22:34 +0300

    маршрутизация

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
-import Main from "../main-screen/main-screen";
+import MainScreen from "../main-screen/main-screen";
+import LoginScreen from "../login-screen/login-screen";
+import MovieScreen from "../movie-screen/movie-screen";
+import MyListScreen from "../my-list-screen/my-list-screen";
+import AddReviewScreen from "../add-review-screen/add-review-screen";
+import PlayerScreen from "../player-screen/player-screen";
 
 const App = (props) => {
   const {movieDetails} = props;
 
   return (
-    <Main
-      movieDetails = {movieDetails}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen movieDetails = {movieDetails} />
+        </Route>
+
+        <Route exact path="/login">
+          <LoginScreen/>
+        </Route>
+
+        <Route exact path="/mylist">
+          <MyListScreen/>
+        </Route>
+
+        <Route exact path="/films/:id">
+          <MovieScreen/>
+        </Route>
+
+        <Route exact path="/films/:id/review">
+          <AddReviewScreen/>
+        </Route>
+
+        <Route exact path="/player/:id">
+          <PlayerScreen/>
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
   );
 };
 
