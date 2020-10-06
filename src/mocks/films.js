681416c3e000d07f85f700ec62f40f23af8b6041
Evgenia Alfimova <alfimova.evg@gmail.com>
2020-10-06 13:20:17 +0300

    add films mocks

---
+++ src/mocks/films.js
@@ -0,0 +1,163 @@
+export default [
+    {
+      image:
+      {
+        preview: `img/aviator.jpg`,
+        poster: `img/aviator.jpg`
+      },
+      title: `Aviator`,
+      genre: `Biography, Drama`,
+      releaseYear: 2004,
+      promo: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+      rating: {
+        score: 8.9,
+        level: `Very good`,
+        countOfVotes: 843,
+      },
+      description: `A biopic depicting the early years of legendary Director and aviator Howard Hughes' career from the late 1920s to the mid 1940s.`,
+      director: `Martin Scorsese`,
+      starring: [`Leonardo DiCaprio`, `Cate Blanchett`, `Kate Beckinsale`],
+      duration: `2h 50min`
+    },
+    {
+      image:
+      {
+        preview: `img/bohemian-rhapsody.jpg`,
+        poster: `img/bohemian-rhapsody.jpg`
+      },
+      title: `Bohemian Rhapsody`,
+      genre: `Biography, Drama, Music`,
+      releaseYear: 2018,
+      promo: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+      rating: {
+        score: 8.0,
+        level: `Good`,
+        countOfVotes: 344,
+      },
+      description: `The story of the legendary British rock band Queen and lead singer Freddie Mercury, leading up to their famous performance at Live Aid (1985)`,
+      director: `Bryan Singer`,
+      starring: [`Rami Malek`, `Lucy Boynton`, `Gwilym Lee`],
+      duration: `2h 14min`
+    },
+    {
+      image:
+      {
+        preview: `img/revenant.jpg`,
+        poster: `img/revenant.jpg`
+      },
+      title: `Revenant`,
+      genre: `Action, Adventure, Drama`,
+      releaseYear: 2015,
+      promo: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+      rating: {
+        score: 8.0,
+        level: `Good`,
+        countOfVotes: 1629,
+      },
+      description: `A frontiersman on a fur trading expedition in the 1820s fights for survival after being mauled by a bear and left for dead by members of his own hunting team`,
+      director: `Alejandro G. Iñárritu`,
+      starring: [`Leonardo DiCaprio`, `Tom Hardy`, `Will Poulter`],
+      duration: `2h 36min`
+    },
+    {
+      image:
+      {
+        preview: `img/pulp-fiction.jpg`,
+        poster: `img/pulp-fiction.jpg`
+      },
+      title: `Pulp Fiction`,
+      genre: `Crime, Drama`,
+      releaseYear: 1994,
+      promo: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+      rating: {
+        score: 8.9,
+        level: `Very Good`,
+        countOfVotes: 1778,
+      },
+      description: `The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption`,
+      director: `Quentin Tarantino`,
+      starring: [`John Travolta`, `Uma Thurman`, `Samuel L. Jackson`],
+      duration: `2h 34min`
+    },
+    {
+      image:
+      {
+        preview: `img/mindhunter.jpg`,
+        poster: `img/mindhunter.jpg`
+      },
+      title: `Mindhunter`,
+      genre: `Crime, Drama, Thriller`,
+      releaseYear: 2017,
+      promo: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+      rating: {
+        score: 8.7,
+        level: `Very good`,
+        countOfVotes: 654,
+      },
+      description: `Set in the late 1970s, two FBI agents are tasked with interviewing serial killers to solve open cases`,
+      director: `Joe Penhall`,
+      starring: [`Jonathan Groff`, `Holt McCallany`, `Anna Torv`],
+      duration: `1h`
+    },
+    {
+      image:
+      {
+        preview: `img/war-of-the-worlds.jpg`,
+        poster: `img/war-of-the-worlds.jpg`
+      },
+      title: `War of the Worlds`,
+      genre: `Adventure, Sci-Fi, Thriller`,
+      releaseYear: 2005,
+      promo: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+      rating: {
+        score: 6.5,
+        level: `Good`,
+        countOfVotes: 410,
+      },
+      description: `As Earth is invaded by alien tripod fighting machines, one family fights for survival in this sci-fi action film`,
+      director: `Steven Spielberg`,
+      starring: [`Tom Cruise`, `Dakota Fanning`, `Tim Robbins`],
+      duration: `1h 27min`
+    },
+    {
+      image:
+      {
+        preview: `img/seven-years-in-tibet.jpg`,
+        poster: `img/seven-years-in-tibet.jpg`
+      },
+      title: `Seven Years in Tibet`,
+      genre: `Adventure, Biography, Drama`,
+      releaseYear: 1997,
+      promo: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+      cover: `img/bg-the-grand-budapest-hotel.jpg`,
+      rating: {
+        score: 7.1,
+        level: `Good`,
+        countOfVotes: 340,
+      },
+      description: `True story of Heinrich Harrer, an Austrian mountain climber who became friends with the Dalai Lama at the time of China's takeover of Tibet`,
+      director: `Jean-Jacques Annaud`,
+      starring: [`Brad Pitt`, `David Thewlis`, `BD Wong`],
+      duration: ` 2h 16min`
+    },
+    {
+      image:
+      {
+        preview: `img/moonrise-kingdom.jpg`,
+        poster: `img/moonrise-kingdom.jpg`
+      },
+      title: `Moonrise Kingdom`,
+      genre: `Comedy, Drama, Romance`,
+      releaseYear: 2012,
+      promo: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+      rating: {
+        score: 7.7,
+        level: `Good`,
+        countOfVotes: 260,
+      },
+      description: `A pair of young lovers flee their New England town, which causes a local search party to fan out to find them`,
+      director: `Wes Anderson`,
+      starring: [`Jared Gilman`, `Kara Hayward`, `Bruce Willis`],
+      duration: `1h 34min`
+    },
+]
