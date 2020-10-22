e75d910e729d333d17fd4ffd548bf99f4e623b16
Evgenia Alfimova <alfimova.evg@gmail.com>
2020-10-22 13:42:05 +0300

    добавлен arrayOf для genre

--- src/components/movie-reviews/movie-reviews.jsx
+++ src/components/movie-reviews/movie-reviews.jsx
@@ -7,18 +7,19 @@ const MovieReviews = (props) => {
   return (
     <div className="movie-card__reviews movie-card__row">
       <div className="movie-card__reviews-col">
-        {reviews.map((review) => <div key={review.author} className="review">
-          <blockquote className="review__quote">
-            <p className="review__text">{review.text}</p>
+        {reviews.map((review) => (
+          <div key={review.author} className="review">
+            <blockquote className="review__quote">
+              <p className="review__text">{review.text}</p>
 
-            <footer className="review__details">
-              <cite className="review__author">{review.author}</cite>
-              <time className="review__date" dateTime="2016-12-24">{review.date}</time>
-            </footer>
-          </blockquote>
+              <footer className="review__details">
+                <cite className="review__author">{review.author}</cite>
+                <time className="review__date" dateTime="2016-12-24">{review.date}</time>
+              </footer>
+            </blockquote>
 
-          <div className="review__rating">{review.rating}</div>
-        </div>)}
+            <div className="review__rating">{review.rating}</div>
+          </div>))}
       </div>
     </div>
   );
