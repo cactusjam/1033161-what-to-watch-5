ec0a4d833d49f13300551f33ef243873360fed4b
Evgenia Alfimova <alfimova.evg@gmail.com>
2020-10-24 19:15:39 +0300

    добавлен ActionCreator

--- src/store/action.js
+++ src/store/action.js
@@ -2,3 +2,14 @@ export const ActionType = {
   CHANGE_FILTER_BY_GENRE: `CHANGE_FILTER_BY_GENRE`,
   GET_FILMS_BY_GENRE: `GET_FILMS_BY_GENRE`,
 };
+
+export const ActionCreator = {
+  changeGenreFilter: (filter)=>({
+    type: ActionType.CHANGE_FILTER_BY_GENRE,
+    payload: filter
+  }),
+  getFilmsByGenre: (films, genreFilter)=>({
+    type: ActionType.GET_FILMS_BY_GENRE,
+    payload: {films, genreFilter}
+  })
+};
