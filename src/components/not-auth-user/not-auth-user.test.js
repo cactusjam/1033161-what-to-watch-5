2b54df9721653227373853dc25bd30b5ad664a20
Evgenia Alfimova <alfimova.evg@gmail.com>
2020-11-20 23:52:35 +0200

    тесты и правки по ним 2

---
+++ src/components/not-auth-user/not-auth-user.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import NotAuthUser from "./not-auth-user";
+import {MemoryRouter} from "react-router-dom";
+
+describe(`NotAuthUser`, () => {
+  it(`Should NotAuthUser render correctly`, () => {
+    const tree = renderer
+    .create(
+        <MemoryRouter>
+          <NotAuthUser/>,
+        </MemoryRouter>
+    )
+  .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
